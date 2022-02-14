69b1d1f024f8d53056b7ce8cb020816a5b80b33a
Okabe Rintarou <eliotandelon@gmail.com>
2022-02-13 21:58:54 -0500

    change option menu

--- ProgrammingInBash/99_back_up_pg.sh
+++ ProgrammingInBash/99_back_up_pg.sh
@@ -1,49 +1,42 @@
 #! /bin/bash
 
 option=''
-backup_name=''
-password=""
-
 
 while :
 do
-    #Limpiar la pantalla
     clear
-    #Desplegar el menú de opciones
     echo "_________________________________________"
-    echo "PGUTIL - Programa de Utilidad de Postgres"
+    echo "PGUTIL - Program Utility for Postgre Sql "
     echo "_________________________________________"
-    echo "                MENÚ PRINCIPAL           "
+    echo "                  MAIN MENU              "
     echo "_________________________________________"
-    echo "1. Instalar Postgres"
-    echo "2. Desinstalar Postgres"
-    echo "3. Sacar un respaldo"
-    echo "4. Restar respaldo"
-    echo "5. Salir"
+    echo "1. Install Postgres"
+    echo "2. UnInstall Postgres"
+    echo "3. Do Backup"
+    echo "4. Show Backup"
+    echo "5. exit"
 
-    #Leer los datos del usuario - capturar información
-    read -n1 -p "Ingrese una opción [1-5]:" opcion
+    read -n1 -p "Choose the option [1-5]: " opcion
 
-    #Validar la opción ingresada
     case $opcion in
         1)
-            echo -e "\nInstalar postgres....."
+            echo -e "\nInstalling postgres....."
             sleep 3
             ;;
         2) 
-            echo -e "\nDesinstalar postgres...."
+            echo -e "\nUninstalling postgres...."
             sleep 3
             ;;
         3) 
-            echo -e "\nSacar respaldo..."
+            echo -e "\nDoing Backup..."
             sleep 3
             ;;
         4) 
-            echo -e "\nRestaurar respaldo..."
+            echo -e "\nShowing Backup..."
             sleep 3
             ;;
         5)  
-            echo -e "\nSalir del Programa"
+            echo -e "\nExit Program"
             exit 0
             ;;
     esac
