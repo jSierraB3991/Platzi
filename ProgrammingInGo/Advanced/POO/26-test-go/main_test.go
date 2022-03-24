ba8bbe5aeb022bddb5a26ef0bf20183cb88b4558
Juan David Sierra <juan.sierra@zabud.com.co>
2022-03-23 22:51:29 -0500

    coverage in test in go

--- ProgrammingInGo/Advanced/POO/26-test-go/main_test.go
+++ ProgrammingInGo/Advanced/POO/26-test-go/main_test.go
@@ -6,7 +6,7 @@ func TestSum(t *testing.T) {
     total := Sum(5 ,5)
 
     if total != 10 {
-        t.Errorf("Syn wa incorrec, go %d expect %d", total, 10)
+        t.Errorf("Sum was incorrec, go %d expect %d", total, 10)
     }
 }
 
@@ -23,6 +23,23 @@ func TestSumII(t *testing.T) {
         total := Sum(item.a, item.b)
         if total != item.n {
             t.Errorf("Sum was incorrect, go %d, expect %d", total, item.n)
+        }
+    }
+}
+
+func TestGetMax(t * testing.T) {
+    tables := [] struct {
+        a int
+        b int
+        n int
+    }{
+        {3,2,3},{5,4,5},{9,4,9},{23,35,35},
+    }
+
+    for _, item := range tables {
+        more := GetMax(item.a, item.b)
+        if more != item.n {
+            t.Errorf("Get max is incorrect, got %d expect %d", more, item.n)
         }
     }
 }
