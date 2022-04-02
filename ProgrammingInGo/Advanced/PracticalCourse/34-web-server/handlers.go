f34b20e864a51b4d1d5683df7851520db3627372
Juan David Sierra <juan.sierra@zabud.com.co>
2022-04-02 14:31:18 -0500

    add dato for client, and recept

--- ProgrammingInGo/Advanced/PracticalCourse/34-web-server/handlers.go
+++ ProgrammingInGo/Advanced/PracticalCourse/34-web-server/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
     "net/http"
     "fmt"
+    "encoding/json"
 )
 
 func HandlerRoot(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +12,15 @@ func HandlerRoot(w http.ResponseWriter, r *http.Request) {
 
 func HandlerHome(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "This is th API Enpoint")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+    decoder := json.NewDecoder(r.Body)
+    var metadata Metadata
+    err := decoder.Decode(&metadata)
+    if err != nil {
+        fmt.Fprintf(w, "error: %v\n", err)
+        return
+    }
+    fmt.Fprintf(w, "Payload %v\n", metadata)
 }
--- ProgrammingInGo/Advanced/PracticalCourse/34-web-server/main.go
+++ ProgrammingInGo/Advanced/PracticalCourse/34-web-server/main.go
@@ -2,7 +2,8 @@ package main
 
 func main() {
     server := NewServer(":5000")
-    server.Handle("/", HandlerRoot)
-    server.Handle("/api", server.AddMiddleware(HandlerHome, CheckAuth(), Logging()))
+    server.Handle("GET", "/", HandlerRoot)
+    server.Handle("POST", "/api", server.AddMiddleware(HandlerHome, CheckAuth(), Logging()))
+    server.Handle("POST", "/create", PostRequest)
     server.Listen()
 }
