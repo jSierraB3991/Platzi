75da374e2fdbeb8b4731e940e0ab7394eadcd6da
Juan David Sierra <judas3991@gmail.com>
2022-10-02 12:49:02 -0500

    fix test with mockito

--- Java/testing/src/main/java/org/jsierra3991/payments/service/PaymentProcessor.java
+++ Java/testing/src/main/java/org/jsierra3991/payments/service/PaymentProcessor.java
@@ -2,7 +2,6 @@ package org.jsierra3991.payments.service;
 
 import lombok.RequiredArgsConstructor;
 import org.jsierra3991.payments.models.PaymentRequest;
-import org.jsierra3991.payments.models.PaymentResponse;
 import org.jsierra3991.payments.models.PaymentStatus;
 
 @RequiredArgsConstructor
@@ -11,7 +10,7 @@ public class PaymentProcessor {
     private final PaymentGateWay paymentGateWay;
 
     public boolean makePayment(double amount) {
-        return paymentGateWay.requestPayment(PaymentRequest.builder().amount(amount).build())
-                .equals(PaymentResponse.builder().status(PaymentStatus.OK).build());
+        var paymentStatus = paymentGateWay.requestPayment(PaymentRequest.builder().amount(amount).build()).getStatus();
+        return paymentStatus.equals(PaymentStatus.OK);
     }
 }
--- Java/testing/src/test/java/org/jsierra3991/payments/service/PaymentProcessorTest.java
+++ Java/testing/src/test/java/org/jsierra3991/payments/service/PaymentProcessorTest.java
@@ -1,17 +1,23 @@
 package org.jsierra3991.payments.service;
 
-
-import org.jsierra3991.payments.models.PaymentRequest;
 import org.jsierra3991.payments.models.PaymentResponse;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.jsierra3991.payments.models.PaymentStatus.ERROR;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.jsierra3991.payments.models.PaymentStatus.OK;
 
+//org.mockito.junit.jupiter.MockitoExtension;
+//org.junit.jupiter.api.extension.ExtendWith;
+//jupiter @ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 public class PaymentProcessorTest {
 
     @InjectMocks
@@ -25,5 +31,13 @@ public class PaymentProcessorTest {
                 .thenReturn(PaymentResponse.builder().status(OK).build());
         boolean result = paymentProcessor.makePayment(1000);
         assertTrue(result);
+    }
+
+    @Test
+    public void payment_is_worng(){
+        when(paymentGateWay.requestPayment(any()))
+                .thenReturn(PaymentResponse.builder().status(ERROR).build());
+        boolean result = paymentProcessor.makePayment(1000);
+        assertFalse(result);
     }
 }
